571603ac61f2853835e723d68f56bb20bd38702a
stepozer <stepozer@gmail.com>
2015-06-06 18:49:49 +0300

    PHP syntax and CS check ready

--- vcs_sniffer/__init__.py
+++ vcs_sniffer/__init__.py
@@ -2,12 +2,16 @@ import vcs_sniffer
 
 # Repository precommit hook
 def precommit(ui, repo, **kwargs):
-    sniffer = vcs_sniffer.VcsSniffer()
-
-    for file in repo[None].files():
-        ui.warn('\n '+file+' \n')
-
-    return 1
+    try:
+        sniffer = vcs_sniffer.VcsSniffer()
+        for file in repo[None].files():
+            sniffer.sniff(repo.root+'/'+file)
+        return 0
+    except vcs_sniffer.VcsSnifferException as e:
+        ui.warn('\n------------------------------------\n')
+        ui.warn(str(e))
+        ui.warn('\n------------------------------------\n')
+        return 1
 
 # Configure repository-specific hooks
 def reposetup(ui, repo):
--- vcs_sniffer/vcs_sniffer.py
+++ vcs_sniffer/vcs_sniffer.py
@@ -27,41 +27,33 @@ class VcsSniffer:
     # def __init__(self):
         # self.options = {}
 
+    # Run CLI command
     def run_command(self, command):
         p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
         return output
 
+    # Return true if file contain PHP code
     def is_php_file(self, file):
         return bool(re.match('.*\.('+')|('.join(self.options['php']['extensions'])+')', file))
 
     # Check PHP syntax
     def check_php_syntax(self, file):
-        # Check file extension and file existence
-        if not self.is_php_file(file) or not os.path.isfile(file):
-            return
-
-        # Run check PHP syntax command
         output = self.run_command(self.options['php']['check_syntax'].replace('%file%', file))
-
-        # Is PHP file valid?
         if not re.match('No syntax errors detected', output):
             raise VcsSnifferException('PHP syntax error in '+file+': '+output)
 
     # Check PHP coding standard
     def check_php_cs(self, file):
-        # Check file extension and file existence
-        if not self.is_php_file(file) or not os.path.isfile(file):
-            return
-
-        # Run PHP coding standard check
         output = self.run_command(self.options['php']['check_cs'].replace('%file%', file))
-
-        # Is PHP file coding standard valid?
         if output != '':
             raise VcsSnifferException("PHP coding standard errors: \n"+output)
 
-# sniffer = VcsSniffer()
+    # Sniff current file
+    def sniff(self, file):
+        if not os.path.isfile(file):
+            return
 
-# sniffer.check_php_syntax('/var/www/vcs_commit_sniffer/test.php')
-# sniffer.check_php_cs('/var/www/vcs_commit_sniffer/test.php')
+        if self.is_php_file(file):
+            self.check_php_syntax(file)
+            self.check_php_cs(file)
